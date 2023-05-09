64d3c865dab772509e1d7d872fbe259433524afa
Stefan Jordan <sjordan2010@gmail.com>
2023-05-09 13:16:59 -0400

    combined data with low, high, and conditionals... next is handling override for market suspended and styling

--- src/components/TestFetch.tsx
+++ src/components/TestFetch.tsx
@@ -1,4 +1,4 @@
-import { useQueries, useQuery } from "@tanstack/react-query";
+import { useQueries } from "@tanstack/react-query";
 import { fetchAlternates, fetchProps } from "@/utils/fetchData";
 import { useState } from "react";
 
@@ -30,6 +30,11 @@ export default function TestFetch() {
     line: number;
   };
 
+  type CombinedData = PropData & {
+    lowLine: number;
+    highLine: number;
+  };
+
   type AlternateData = {
     line: number;
     overOdds: number;
@@ -48,7 +53,51 @@ export default function TestFetch() {
 
   if (AltLoading || PropLoading) return <div>Loading...</div>;
 
-  const filteredData: PropData[] = PropQuery.filter(
+  // const filteredData: PropData[] = PropQuery.filter(
+  //   (item: PropData) =>
+  //     (item.playerName.toLowerCase().includes(searchValue.toLowerCase()) ||
+  //       item.teamNickname.toLowerCase().includes(searchValue.toLowerCase())) &&
+  //     (selectedStatType === "all" || item.statType === selectedStatType) &&
+  //     (selectedPosition === "all" || item.position === selectedPosition) &&
+  //     (selectedMarketSuspended === "all" ||
+  //       item.marketSuspended.toString() === selectedMarketSuspended)
+  // );
+
+  const combineData = (propData: PropData[], altData: AlternateData[]): CombinedData[] => {
+    const combined = [];
+
+    for (let i = 0; i < propData.length; i++) {
+      let lowLine = propData[i].line,
+        highLine = propData[i].line,
+        matched = false,
+        suspended = false;
+
+      for (let j = 0; j < altData.length; j++) {
+        if (propData[i].playerId === altData[j].playerId) {
+          if (propData[i].statTypeId === altData[j].statTypeId) {
+            if (propData[i].line === altData[j].line) {
+              matched = true;
+            }
+            if (altData[j].line > highLine) highLine = altData[j].line;
+            if (altData[j].line < lowLine) lowLine = altData[j].line;
+            if (
+              altData[j].overOdds < 0.4 &&
+              altData[j].pushOdds < 0.4 &&
+              altData[j].underOdds < 0.4
+            ) {
+              suspended = true;
+            }
+          }
+        }
+      }
+      if (suspended || !matched) propData[i].marketSuspended = 1;
+      combined.push({ ...propData[i], lowLine: lowLine, highLine: highLine });
+    }
+
+    return combined;
+  };
+
+  const combinedData: CombinedData[] = combineData(PropQuery, AltQuery).filter(
     (item: PropData) =>
       (item.playerName.toLowerCase().includes(searchValue.toLowerCase()) ||
         item.teamNickname.toLowerCase().includes(searchValue.toLowerCase())) &&
@@ -78,14 +127,25 @@ export default function TestFetch() {
     setSearchValue("");
   };
 
+  const handleOverride = (playerID: number, statID: number): void => {
+    console.log("clicked id", playerID, statID);
+    combinedData.forEach((market: CombinedData) => {
+      if (market.playerId === playerID && market.statTypeId === statID) {
+        market.marketSuspended === 1 ? (market.marketSuspended = 0) : (market.marketSuspended = 1);
+      }
+    });
+    console.log("PropQuery", PropQuery);
+  };
+
   return (
     <div>
       <label htmlFor="stat-type">Filter by stat type:</label>
       <select id="stat-type" value={selectedStatType} onChange={handleStatTypeChange}>
         <option value="all">All</option>
-        <option value="assists">Assists</option>
-        <option value="rebounds">Rebounds</option>
         <option value="points">Points</option>
+        <option value="rebounds">Rebounds</option>
+        <option value="assists">Assists</option>
+        <option value="steals">Steals</option>
       </select>
       <label htmlFor="position">Filter by Position: </label>
       <select id="position" value={selectedPosition} onChange={handlePositionChange}>
@@ -124,23 +184,27 @@ export default function TestFetch() {
           <tr>
             <th>Team</th>
             <th>Player</th>
-            <th>Position</th>
-            <th>Stat Type</th>
+            <th>Pos</th>
+            <th>Stat</th>
             <th>Line</th>
-            <th>Market Status</th>
-            <th>HighALT</th>
-            <th>LowALT</th>
+            <th>Low</th>
+            <th>High</th>
+            <th>Market</th>
           </tr>
         </thead>
         <tbody>
-          {filteredData.map((item) => (
+          {combinedData.map((item) => (
             <tr key={`${item.playerId}-${item.statTypeId}`}>
               <td>{item.teamAbbr}</td>
               <td>{item.playerName}</td>
               <td>{item.position}</td>
-              <td>{item.statType}</td>
+              <td>{item.statType.charAt(0).toUpperCase() + item.statType.slice(1)}</td>
               <td>{item.line}</td>
-              <td>{item.marketSuspended}</td>
+              <td>{item.lowLine}</td>
+              <td>{item.highLine}</td>
+              <td onClick={() => handleOverride(item.playerId, item.statTypeId)}>
+                {item.marketSuspended === 1 ? "Suspended" : "Open"}
+              </td>
             </tr>
           ))}
         </tbody>
@@ -148,3 +212,9 @@ export default function TestFetch() {
     </div>
   );
 }
+
+// find all markets in alternates data where the playerID and statTypeID is the same
+// find the highest and lowest line out of those and print to table
+// if statTypeID or playerID doesn't exist within alternates => market is SUSPENDED
+// if marketSuspended = 1 => market is SUSPENDED
+// if over, under, and push are all under 0.4 => market is SUSPENDED
